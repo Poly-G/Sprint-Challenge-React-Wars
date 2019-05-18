890211b28e447e38c4e0dd80121af3bd7f5f9185
Poly Goveia <poly.goveia@gmail.com>
2019-05-18 09:59:13 -0700

    display characters

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const Character = () => {
+
+const Character = props => {
   return (
     <div>
-      
+      {props.starwarsChars.map((char) => (
+        <div className="card">
+          <h3>{char.name}</h3>
+          <p>
+            <strong>Gender:</strong> {char.gender}
+          </p>
+          <p>
+            <strong>Height:</strong> {char.height}
+          </p>
+          <p>
+            <strong>Mass:</strong> {char.mass}
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
-
 export default Character
